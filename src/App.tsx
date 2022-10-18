955a148377f2348fdb8ed6182044311b2c59635b
majoferenc <44170671+majoferenc@users.noreply.github.com>
2022-10-18 10:38:34 +0200

    fix: graphql calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+const GET_SALATS = gql`
+  {
+    salats {
+      id
+      name
+      type
+    }
+  }
+`;
 
 function App() {
-  return (
+  const { loading, error, data } = useQuery(GET_SALATS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.salats.map(({ id, name, type }: any) => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div key={id}>
+      <h3>{name}</h3>
+      <p>{type}</p>
+      <br />
+      </div>
+    </header>
     </div>
-  );
+  ));
 }
 
 export default App;
